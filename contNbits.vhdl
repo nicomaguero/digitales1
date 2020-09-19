c05b7a3efd99a342eefde1c4466d99dbc63d5365
nicomaguero <nicomaguero@gmail.com>
2020-09-19 20:07:39 -0300

    Actualizacion de codigo

--- contNbits.vhdl
+++ contNbits.vhdl
@@ -1,26 +1,27 @@
---Contador N-bits Digitales 1
+--Contador N-bits Digitales 
 --
 --Fecha 14/9/20
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity cont_Nbits is
-	generic(N: natural);
+	generic(N: natural); --Variable natural generica, el valor lo determino de manera externa
 	port(
-		clk: in std_logic;
-		rst: in std_logic;
+		clk: in std_logic;                      --Defino los puertos del bloque, entradas y salidas
+		rst: in std_logic;				    	--Variable del standard IEEE, "0" o "1"
 		ena: in std_logic;
-		Q: out std_logic_vector(N-1 downto 0)
+		Q: out std_logic_vector(N-1 downto 0)   --Salida tipo vector 
 	);
 
 end;
 
-architecture cont_Nbits_arq of cont_Nbits is
-	signal d_aux: std_logic_vector(N-1 downto 0);
+architecture cont_Nbits_arq of cont_Nbits is  				--Defino la arquitectura dentro de la entidad 
+
+	signal d_aux: std_logic_vector(N-1 downto 0);           --Defino variable tipo señal, para conexiones internas
 	signal q_aux: std_logic_vector(N-1 downto 0);
 	signal and_aux: std_logic_vector(N downto 0);
 
-	component ffd is
+	component ffd is									--Se declara el componete a utilizar (Subsistema)
 		port(
 			clk: in std_logic;
 			arst: in std_logic;
@@ -32,11 +33,11 @@ architecture cont_Nbits_arq of cont_Nbits is
 	end component;
 	
 
-	begin
+	begin      								--Comienzan las sentencias
 
-	and_aux(0) <= '1';
-	gg: for i in 0 to N-1 generate
-    ffd_inst : ffd port map(
+	and_aux(0) <= '1';						
+	gg: for i in 0 to N-1 generate			--creo un for para formar un array de interconexiones 
+    ffd_cont : ffd port map(				--mapeo de los puertos ffd component con las señales internas y las entradas del bloque 
 					clk => clk,
 					arst => '0',
 					srst => rst,
@@ -47,6 +48,6 @@ architecture cont_Nbits_arq of cont_Nbits is
 	d_aux(i) <= and_aux(i) xor q_aux(i);
 	and_aux(i+1) <= q_aux(i) and and_aux(i);
 	end generate;
-	Q <= q_aux;
+	Q <= q_aux;								--Asigno a la salida del bloque los valores obtenidos por el for de cada ffd
 	
 end;
